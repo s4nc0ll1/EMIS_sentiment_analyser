8de161c634ff772c161297c52b31f6626bcf2a67
Javier Gomez <jagomez@isimarkets.com>
2025-08-12 21:58:11 +0300

    core: update init file into emis_downloader

--- core/emis_downloader/__init__.py
+++ core/emis_downloader/__init__.py
@@ -1,4 +1,11 @@
 from .models import EnrichedDoc, RawDoc, SearchContext
 from .sentiment import VaderSentimentScorer
+from .storage import SQLiteStore
 
-__all__ = ["RawDoc", "EnrichedDoc", "SearchContext", "VaderSentimentScorer"]
+__all__ = [
+    "RawDoc",
+    "EnrichedDoc",
+    "SearchContext",
+    "VaderSentimentScorer",
+    "SQLiteStore",
+]
